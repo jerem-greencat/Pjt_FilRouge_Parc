745a588a3d867d61479d45c52631550c783f4aa2
jerem-greencat <82501643+jerem-greencat@users.noreply.github.com>
2022-01-07 16:30:35 +0100

    test upload file

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const io = require('socket.io')(server);
 const mysql = require('mysql');
 const { query } = require("express");
 let bodyParser = require("body-parser");
-const jwt = require ('jsonwebtoken');
-const cookieParser = require ('cookie-parser');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 const SECRET = 'token';
 
@@ -38,11 +38,11 @@ con.connect(function (err) {
             res.render(__dirname + '/index.ejs');
         })
 
-        .get('/attractions', (req,res) => {
+        .get('/attractions', (req, res) => {
             res.render(__dirname + '/public/pages/attractions.ejs');
         })
 
-        .get('/seloger', (req, res)=> {
+        .get('/seloger', (req, res) => {
             res.render(__dirname + '/public/pages/seloger.ejs');
         })
 
@@ -79,9 +79,9 @@ con.connect(function (err) {
         })
 
         .get('/compte', (req, res) => {
-            if (userConected.length == 0){
+            if (userConected.length == 0) {
                 res.render(__dirname + '/public/pages/connection.ejs');
-            }else{
+            } else {
                 res.render(__dirname + '/public/pages/moncompte.ejs');
             }
         })
@@ -92,7 +92,7 @@ con.connect(function (err) {
 
         .get('/error/:id', (req, res) => {
             let error, returnPath, returnMsg;
-            switch(parseInt(req.params.id)) {
+            switch (parseInt(req.params.id)) {
                 case 1:
                     error = "Ce mail est déjà pris.";
                     returnPath = "/inscription";
@@ -140,19 +140,19 @@ con.connect(function (err) {
             } else {
                 query = `SELECT admin_id FROM admin WHERE admin_mail = "${mail}" LIMIT 1;`;
             }
-            
-            if (adminC == 'on' && passAdmin == adminPassword){                
+
+            if (adminC == 'on' && passAdmin == adminPassword) {
                 insert = `INSERT INTO admin (admin_firstname, admin_lastname, admin_birth, admin_mail, admin_phone, admin_password) VALUES ("${fName}", "${lName}", "${birth}", "${mail}", "${phone}","${pwd}");`;
 
 
-            } else{                
+            } else {
                 insert = `INSERT INTO user (user_firstname, user_lastname, user_birth, user_mail, user_phone, user_password) VALUES ("${fName}", "${lName}", "${birth}", "${mail}", "${phone}","${pwd}");`;
 
             }
 
             con.query(query, (errorSlct, users) => {
                 if (errorSlct) throw errorSlct;
-                if(users.length !== 0) {
+                if (users.length !== 0) {
                     console.log(users)
                     res.redirect('/error/1');
                 } else {
@@ -166,7 +166,7 @@ con.connect(function (err) {
                             res.redirect('/error/2');
                         }
                     });
-                } 
+                }
             });
         })
 
@@ -175,85 +175,125 @@ con.connect(function (err) {
             const mail = req.body.mail;
             const pwd = req.body.password;
             const adminC = req.body.adminCase;
-            const passAdmin =  req.body.passAdmin;
+            const passAdmin = req.body.passAdmin;
 
             let query;
             // let update;
-            
-            if(adminC !== 'on'){
-                query = `SELECT user_id FROM user WHERE user_mail = "${mail}" AND user_password = "${pwd}" AND connected = 0 LIMIT 1;`;
-                
 
-            }else if (adminC == 'on' && passAdmin == adminPassword){
+            if (adminC !== 'on') {
+                query = `SELECT user_id FROM user WHERE user_mail = "${mail}" AND user_password = "${pwd}" AND connected = 0 LIMIT 1;`;
+
+
+            } else if (adminC == 'on' && passAdmin == adminPassword) {
                 query = `SELECT admin_id FROM admin WHERE admin_mail = "${mail}" AND admin_password = "${pwd}" AND connected = 0 LIMIT 1;`;
-                
+
             };
 
 
             // Connection user
-            
+
 
             con.query(query, (errorSlct, users) => {
                 if (errorSlct) throw errorSlct;
                 if (users.length === 0) {
                     res.redirect('/error/3');
                 } else {
-                    userId = users[0].user_id;   
-                    adminId = users[0].admin_id; 
-                    if(adminC !== 'on'){
+                    userId = users[0].user_id;
+                    adminId = users[0].admin_id;
+                    if (adminC !== 'on') {
                         // update = `UPDATE user SET connected = 1 WHERE user_id = ${userId};`;
 
-                    userConected.push(userId);
-                    
-                    const token = jwt.sign({
-                        id : userId
-                    }, SECRET, { expiresIn: '3 hours'})  
-                    
-                    res.cookie( "access_token", token)
-                    res.redirect('/');
-                    
-                    
-                    }else if (adminC == 'on' && passAdmin == adminPassword){
+                        userConected.push(userId);
+
+                        const token = jwt.sign({
+                            id: userId
+                        }, SECRET, { expiresIn: '3 hours' })
+
+                        res.cookie("access_token", token)
+                        res.redirect('/');
+
+
+                    } else if (adminC == 'on' && passAdmin == adminPassword) {
                         // update = `UPDATE admin SET connected = 1 WHERE admin_id = ${adminId};`;
                         adminConected.push(adminId);
-                        
-                        const token = jwt.sign({
-                            id : adminId
-                        }, SECRET, { expiresIn: '3 hours'})
-                        return res.json({ access_token : token})
-                        
 
-                    };                
+                        const token = jwt.sign({
+                            id: adminId
+                        }, SECRET, { expiresIn: '3 hours' })
+                        return res.json({ access_token: token })
+
+
+                    };
                     // con.query(update, (error, result) => {
                     //     if (error) throw error;
                     //     res.redirect('/');
                     // });
                 }
             });
-        });
+        })
 
-        // Déconnection user
+        // upload file 
 
-        io.on("disconnection", userId => {
-            let update;
-            console.log(userId);
+        // Have to install multer ==> npm install --save multer
 
-            if (userConected.length !== 0){
-                update = `UPDATE user SET connected = 0 WHERE user_id = ${userId};`;
-                userConected = [];
-            }else if (adminConected.length !== 0){
-                update =  `UPDATE admin SET connected = 0 WHERE admin_id = ${adminId};`;
-                adminConected = [];
+        //  tuto : https://codingstatus.com/how-to-store-image-in-mysql-database-using-node-js/
+        .post('/addFile', (req, res) => {
+            const file = LOAD_FILE(req.body.fileUpload);
+            const fileTitle = req.body.fileTitle;
+            // let insert = `INSERT INTO file (file_name, adress_file) VALUES ("${fileTitle}", "${file}");`;
+
+
+
+            module.exports = {
+                storeImage: uploadFunction(inputValues, callback){
+                // check unique email address
+                const sql = 'SELECT * FROM images WHERE adress_file =?';
+                con.query(sql, inputValues.adress_file, function (err, data, fields) {
+                    if (err) throw err
+                    if (data.length > 1) {
+                        var msg = inputValues.adress_file + " is already exist";
+                    } else {
+                        // save users data into database
+                        var sql = 'INSERT INTO file SET ?';
+                        con.query(sql, inputValues, function (err, data) {
+                            if (err) throw err;
+                        });
+                        var msg = inputValues.image_name + "is uploaded successfully";
+                    }
+                    return callback(msg)
+                })
             }
-            con.query(update, (error, result) => {
-                console.log(userConected);
-                console.log(adminConected);
-            });
+        }
+
+            // con.query(insert, (error, result) => {
+            //     if (result.affectedRows) { 
+            //         res.redirect('/vie');
+            //     } else if (error) throw error;
+            // })
         });
 
+// Déconnection user
+
+io.on("disconnection", userId => {
+    let update;
+    console.log(userId);
+
+    if (userConected.length !== 0) {
+        update = `UPDATE user SET connected = 0 WHERE user_id = ${userId};`;
+        userConected = [];
+    } else if (adminConected.length !== 0) {
+        update = `UPDATE admin SET connected = 0 WHERE admin_id = ${adminId};`;
+        adminConected = [];
+    }
+    con.query(update, (error, result) => {
+        console.log(userConected);
+        console.log(adminConected);
+    });
+});
 
 
-    
-    server.listen(8080);
+
+
+server.listen(8080);
 
 });
